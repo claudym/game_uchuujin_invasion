a396711e56c00a75a8e0e812b727caf89e8581cb
claudym <claudy.m.mejia@gmail.com>
2022-02-25 11:03:03 -0400

    fleet comes down and changes direction

--- code/settings.py
+++ code/settings.py
@@ -14,3 +14,8 @@ class Settings:
     self.bullet_height = 15
     self.bullet_color = (212, 104, 242)
     self.bullets_allowed = 10
+
+    #alien settings
+    self.alien_speed = 1.0
+    self.fleet_drop_speed = 10
+    self.fleet_direction = 1  #1 represents right | -1 represents left
